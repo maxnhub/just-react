6beb604e5e1dda478cc94bbce3d92a1887510029
maksim <maxnikulnikov@gmail.com>
2021-04-25 12:17:28 +0300

    basic_knowledge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import classes from './App.module.scss';
+import Game from './Game/Game';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Counter from './Counter/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            games: [
+                {name: 'DaysGone', genre: 'Post-apocalypse survival'},
+                {name: 'Control', genre: 'Action like Twen Peeks and X-files'},
+                {name: 'Ghost of Tsushima', genre: 'Japanese hero history'}
+            ],
+            pageTitle: 'React components',
+            clicksCounter: 0,
+            showList: false
+        }
+    }
+
+
+
+    handleClick = () => {
+        let clicksCounter = this.state.clicksCounter
+
+        let newClicksCounter = clicksCounter + 1
+
+        this.setState({
+            clicksCounter: newClicksCounter
+        })
+    }
+
+    handleTitle = (newPageTitle) => {
+        this.setState({
+            pageTitle: newPageTitle
+        })
+    }
+
+    toggleListHandler = () => {
+        this.setState({
+            showList: !this.state.showList
+        })
+    }
+
+    onChangeName(name, index) {
+        const game = this.state.games[index]
+        game.name = name
+        const games = [...this.state.games]
+        games[index] = game
+        this.setState({games})
+    }
+
+    deleteHandler = (index) => {
+        const games = [...this.state.games]
+        games.splice(index, 1)
+        this.setState({games})
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    render() {
+
+        const divStyle = {
+            'color': 'red'
+        }
+
+        const games = this.state.games
+
+        return (
+            <div className={classes['App']}>
+                <h1 style={divStyle}>{this.state.pageTitle}</h1>
+                <button onClick={this.handleTitle.bind(this, 'Changed!')}>change title</button>
+                <button onClick={this.toggleListHandler}>toggle list</button>
+
+                {this.state.showList ?
+                    this.state.games.map((game, index)=> {
+                    return (
+                        <ErrorBoundary key={index}>
+                            <Game
+                                name={game.name}
+                                genre={game.genre}
+                                onChangeTitle={() => {this.handleTitle(game.name)}}
+                                onChangeName={event =>{this.onChangeName(event.target.value, index)}}
+                                onDelete={this.deleteHandler.bind(this, index)}
+                            />
+                        </ErrorBoundary>
+                    )
+                }) :
+                    null
+                }
+
+                <button onClick={this.handleClick}>count clicks</button>
+                <p>Clicks counter: {this.state.clicksCounter}</p>
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Game/Game.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './Game.scss';
+import PropTypes from 'prop-types'
+
+class Game extends Component {
+    render() {
+        let inputClasses = ['input'];
+
+        if(this.props.name !== '') {
+            inputClasses.push('green');
+        } else {
+            inputClasses.push('red');
+        }
+
+        if(this.props.name.length > 4) {
+            inputClasses.push('bold');
+        }
+
+        // if(Math.random() > 0.7) {
+        //     throw new Error('Random fail!');
+        // }
+        return (
+            <div className={'Game'}>
+                <h3>Game: {this.props.name}</h3>
+                <p>Genre: {this.props.genre}</p>
+                <input className={inputClasses.join(' ')} type="text" onChange={this.props.onChangeName} value={this.props.name}/>
+                <button onClick={this.props.onChangeTitle}>ChangeTitle</button>
+                <button onClick={this.props.onDelete}>Delete</button>
+            </div>
+        )
+    }
+}
+
+Game.propTypes = {
+    name: PropTypes.string,
+    genre: PropTypes.string,
+    onChangeName: PropTypes.func,
+    onDelete: PropTypes.func
+}
+
+export default Game
